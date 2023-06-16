ecb18aae5a1626e04fdc7201b09268d1fed4a4b0
Naki188 <136823537+Naki188@users.noreply.github.com>
2023-06-16 16:02:17 +0000

    eratistenes zadani

---
+++ prvocisla.py
@@ -0,0 +1,22 @@
+# string "jdkk"
+# integer (int)  čísla 123
+# float desetinná čísla
+#ctrl c zastav
+
+#array[] ohraničení
+#len()
+
+array=[]
+array.append(234)
+print(array)
+
+
+n = 16439094
+# udělej pole celých čisel od 2 po n, z pole vypsat prvočísla
+# array= [1,3,6,7]
+#array[1] = 0 vymění druhý prvek pole za 0
+#eratosterosovo číslo
+#for element in array
+    #print(element)
+    #continue pokračování, když něco tak pokračuj
+    #break zruší cyklus
