3540e4141e8a5119879b7b629e13251e84c69722
Naki188 <136823537+Naki188@users.noreply.github.com>
2023-06-17 11:11:32 +0000

    zprava

--- prvocisla.py
+++ prvocisla.py
@@ -6,17 +6,25 @@
 #array[] ohraničení
 #len()
 
-array=[]
-array.append(234)
-print(array)
+#array=[]
+#array.append(234)
+#print(array)
 
 
-n = 16439094
+#n = 16439094
 # udělej pole celých čisel od 2 po n, z pole vypsat prvočísla
 # array= [1,3,6,7]
 #array[1] = 0 vymění druhý prvek pole za 0
-#eratosterosovo číslo
+#eratosterosovo číslo, síto
 #for element in array
     #print(element)
     #continue pokračování, když něco tak pokračuj
     #break zruší cyklus
+
+def eratosteros(konecnecislicko)
+    pocatecnycislicko = 2
+    for n in range(pocatecnycislicko, konecnecislicko+1)
+    print (eratosteros)
+
+eratosteros(16439094)
+
